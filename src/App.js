466e628433e1d1ee11340361f0543ae48b4c18f8
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-01-19 15:12:15 -0500

    finish simple tailwindcss & react

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages';
+import { Routes, Route } from 'react-router-dom';
+import About from './pages/about';
+import Menu from './pages/menu';
+import Dropdown from './components/Dropdown';
+import Contact from './pages/contact';
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log('i resizeedddd');
+      }
+    };
+    window.addEventListener('resize', hideMenu);
+
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    };
+  });
+
+  return (
+    <>
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
